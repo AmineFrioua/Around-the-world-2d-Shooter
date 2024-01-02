a67e93ee977b5696a5f52ee7742f94ef11a05aed
Mohamed Amine Frioua <aminefrioua@gmail.com>
2024-01-02 16:49:25 +0900

    add collisions for bullet after animations

--- Scripts/Weapons/Base/Bullet.cs
+++ Scripts/Weapons/Base/Bullet.cs
@@ -1,22 +1,38 @@
 using Godot;
 using System;
 
-public partial class Bullet : Area2D
-{	
-	public Vector2 Velocity;
-	[Export]
-	public float Speed= 100;
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		 LookAt(GetGlobalMousePosition());
-	}
+public partial class Bullet: Area2D {
+  public AnimatedSprite2D BulletAnimation;
+  public Vector2 Velocity;
+  [Export]
+  public float Speed = 10;
+  private bool collision = false;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		 Position += Velocity * (float)delta;
-	  	
-	}
+  // Called when the node enters the scene tree for the first time.
+  public override void _Ready() {
+	LookAt(GetGlobalMousePosition());
+	BulletAnimation = GetNode < AnimatedSprite2D >("Bullet");
+	BulletAnimation.AnimationFinished += () => OnAnimationFinished() ;
+	BulletAnimation.Play("default");
+  }
+
+  // Called every frame. 'delta' is the elapsed time since                                                                                                   the previous frame.
+  public override void _Process(double delta) {
+	Position += Velocity * Speed * (float) delta;
+  }
+
+  private void _on_body_entered(Node2D body) {
+	
+	BulletAnimation.Play("collision");
+
+	collision = true;
+  }
+
+	private void OnAnimationFinished() {
+	if (collision){
+			Speed = 0 ;
+	  		QueueFree();
+		}
+  	}
 }
+
